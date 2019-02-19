5182f9da22fa2982cfa8f94e2af3b7e1bbd82cbc
Jaxon Porter <jaxonutah@gmail.com>
2019-02-19 11:35:42 -0700

    Added migration file changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  has_many :notes, dependant: :destroy
+  has_one :address, dependant: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_175242) do
+ActiveRecord::Schema.define(version: 2019_02_19_182509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_addresses_on_contact_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -26,4 +34,14 @@ ActiveRecord::Schema.define(version: 2019_02_19_175242) do
     t.boolean "friend"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_notes_on_contact_id"
+  end
+
+  add_foreign_key "addresses", "contacts"
+  add_foreign_key "notes", "contacts"
 end
